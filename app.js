e8f57d2124fc88f5813e93116259a1a72eaa1578
callmedadaxin <554091944@qq.com>
2019-01-24 19:48:56 +0800

    fix port

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+const path = require("path");
+const port = 3002;
+const proxy = require("http-proxy-middleware");
+
+app.set("views", path.join(__dirname, "./build"));
+app.engine("html", ejs.__express);
+app.set("view engine", "html");
+app.use("/static", express.static(path.join(__dirname, "./build/static")));
+
+app.use(
+  "/game/*",
+  proxy({
+    target: "http://42.159.94.242:8080/",
+    changeOrigin: true
+  })
+);
+
+app.get("/", (req, res) => res.render("index.html"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
