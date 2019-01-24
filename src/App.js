c65a107d96fada4c2b7af6914290be8ab442247e
callmedadaxin <554091944@qq.com>
2019-01-24 17:45:53 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from "react";
 import moment from "moment";
 import "./App.css";
 import { get, post } from "./fetch";
+import logo from "./logo.png";
 
 class App extends Component {
   state = {
     list: [],
-    start: false
+    start: false,
+    limit: 5 * 60 * 1000
   };
   start = () => {
     this.setTime();
+    this.timer = setInterval(() => {
+      this.getRank();
+      const { limit } = this.state;
+      if (limit < 0) {
+        this.stop();
+      }
+      this.setState({
+        limit: limit - 1000
+      });
+    }, 1000);
+  };
+  refresh = () => {
     this.timer = setInterval(() => {
       this.getRank();
     }, 1000);
@@ -37,35 +51,47 @@ class App extends Component {
   getRank() {
     get("/game/rank").then((res = []) => {
       this.setState({
-        list: res.slice(0, 5)
+        list: res.slice(0, 3)
       });
     });
   }
   render() {
-    const { list, start } = this.state;
+    const { list, start, limit } = this.state;
+    let time = parseInt((limit % 60000) / 1000);
+    if (time < 0) {
+      time = 0;
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img
-            src={
-              "https://threatbook.cn/assets/68325bd5190b80a6e59594df2f70a51a.svg"
-            }
-            className="App-logo"
-            alt="logo"
-          />
+        <img
+          src={
+            "https://threatbook.cn/assets/68325bd5190b80a6e59594df2f70a51a.svg"
+          }
+          className="App-logo"
+          alt="logo"
+        />
+        <div className="left">
           <h2>游戏排行榜</h2>
+          <div className="time">
+            0{parseInt(limit / 60000)}:{time < 10 ? "0" + time : time}
+          </div>
           <div className="button-wrap">
             {start ? "" : <button onClick={this.start}>点击开始</button>}
             <button onClick={this.stop}>结束</button>
+            <button onClick={this.refresh}>刷新</button>
           </div>
-          <ul>
+
+          <ol>
             {list.map(item => (
               <li key={item.uuid}>
-                {item.nickName}: {item.useTime}秒
+                {item.nickName}: {(item.useTime / 1000).toFixed(2)}秒
               </li>
             ))}
-          </ul>
-        </header>
+          </ol>
+        </div>
+        <div className="right">
+          <img className="logo" src={logo} />
+        </div>
       </div>
     );
   }
